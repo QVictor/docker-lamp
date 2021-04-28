ea978cc25c7a6b7c042cc493cf0fc4cd6848f367
QVictor <QQlogin@yandex.ru>
2021-04-28 17:41:47 +0300

    create autoloader

---
+++ app/components/Autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+spl_autoload_register('simple_autoloader');
+
+function simple_autoloader($class_name) {
+    $array_paths = array(
+      '/models/',
+      '/components/',
+      '/controllers/',
+    );
+
+    foreach ($array_paths as $path) {
+        $path = ROOT . $path . $class_name . '.php';
+        if (is_file($path)) {
+            include_once $path;
+        }
+    }
+}
